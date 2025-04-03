4a15cc1edf7f9372584e59bb49a9bdedb2fe5854
Robin <loeveet@gmail.com>
2025-04-03 09:23:27 +0200

    create emailservice and add verification bool and verificationToken to user

--- FamilyCalender.Core/Models/Entities/User.cs
+++ FamilyCalender.Core/Models/Entities/User.cs
@@ -5,11 +5,12 @@ namespace FamilyCalender.Core.Models.Entities
     public class User
     {
 		public int Id { get; set; }
-		public string Username { get; set; } = string.Empty;
 		public string PasswordHash { get; set; }
 		public string Email { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public bool IsVerified { get; set; }
+        public string VerificationToken { get; set; }
 
         public ICollection<CalendarAccess> CalendarAccesses { get; set; } = [];
         public ICollection<Calendar> OwnedCalendars { get; set; } = [];
