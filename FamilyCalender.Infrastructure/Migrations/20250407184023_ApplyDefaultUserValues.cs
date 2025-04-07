4c10bba07f736f27fbdb8fb7e7aa8c40220aecb3
Mikael Lennander <mikael@goldenedi.com>
2025-04-07 20:44:07 +0200

    task: migrationscript for applying default values on existing users

---
+++ FamilyCalender.Infrastructure/Migrations/20250407184023_ApplyDefaultUserValues.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyCalender.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyDefaultUserValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Sqlite = DATETIME('now'), SQL = GetUtcDate()
+            migrationBuilder.Sql($"Update Users Set IsVerified = 1, VerificationDateUtc = DATETIME('now')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
