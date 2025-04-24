192ea63c0ae731920fa1d41725bf4b57fc7c0fea
Mikael Lennander <mikael@goldenedi.com>
2025-04-24 13:08:21 +0200

    new color picker do loook good on any device desktop or mobile - fuck you built in dropdown designs mother fuckers

---
+++ FamilyCalender.Web/wwwroot/js/eventColorPickerHelper.js
@@ -0,0 +1,38 @@
+﻿var eventColorPickerHelper = function () {
+
+    var _hiddenColorInputFieldId = void 0;
+
+    function init(hiddenColorInputFieldId) {
+        _hiddenColorInputFieldId = hiddenColorInputFieldId;
+    }
+
+    //Color DropDown logic
+    function toggleDropdown(event) {
+        const dropdown = event.currentTarget;
+        const options = dropdown.querySelector('.options');
+
+        // Toggle dropdown visibility
+        options.style.display = options.style.display === 'block' ? 'none' : 'block';
+
+        // Add click event to options
+        options.querySelectorAll('div').forEach(option => {
+            option.onclick = () => {
+                const selected = dropdown.querySelector('.selected');
+                const hiddenInput = document.getElementById(_hiddenColorInputFieldId);
+
+                selected.innerHTML = option.innerHTML;
+                hiddenInput.value = option.dataset.color;
+
+            };
+        });
+
+        // Stop event from closing immediately
+        event.stopPropagation();
+    }
+
+
+    return {
+        Init: init,
+        ToggleDropdown: toggleDropdown
+    };
+}();
