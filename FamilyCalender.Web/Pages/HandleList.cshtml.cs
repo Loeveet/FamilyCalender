965098c11445e717e2829b05686592e4aa5064a7
Robin <loeveet@gmail.com>
2025-06-05 10:01:26 +0200

    add drag and drop to lists

--- FamilyCalender.Web/Pages/HandleList.cshtml.cs
+++ FamilyCalender.Web/Pages/HandleList.cshtml.cs
@@ -1,16 +1,22 @@
 using FamilyCalender.Core.Interfaces.IServices;
 using FamilyCalender.Core.Models.Entities;
+using FamilyCalender.Infrastructure.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FamilyCalender.Web.Pages;
-[IgnoreAntiforgeryToken]
+//[IgnoreAntiforgeryToken]
+[ValidateAntiForgeryToken]
 
-public class HandleListModel(IUserListService userListService, IAuthService authService) : BasePageModel(authService)
+
+public class HandleListModel(IUserListService userListService, IAuthService authService, ApplicationDbContext context) : BasePageModel(authService)
 {
     private readonly IUserListService _userListService = userListService;
+	private readonly ApplicationDbContext _context = context;
 
-    [BindProperty(SupportsGet = true)]
+	[BindProperty(SupportsGet = true)]
     public int ListId { get; set; }
     [BindProperty(SupportsGet = true)]
     public string? CalendarName { get; set; }
@@ -26,8 +32,14 @@ public class HandleListModel(IUserListService userListService, IAuthService auth
         public int ItemId { get; set; }
     }
     public record DeleteItemDto(int ItemId);
+	public class UpdateOrderDto
+	{
+		public int ListId { get; set; }
+		public List<int> SortedIds { get; set; }
+	}
 
-    public async Task<IActionResult> OnGetAsync()
+
+	public async Task<IActionResult> OnGetAsync()
     {
         var user = await GetCurrentUserAsync();
         if (user == null)
@@ -86,6 +98,30 @@ public class HandleListModel(IUserListService userListService, IAuthService auth
 
         return new JsonResult(new { success = true });
     }
+	public async Task<IActionResult> OnPostUpdateOrderAsync([FromBody] UpdateOrderDto data)
+	{
+		var userId = await GetCurrentUserIdAsync();
+		if (userId == null)
+			return Unauthorized();
 
+		if (data == null || data.SortedIds == null)
+			return BadRequest();
+
+		var userList = await _userListService.GetListByIdAsync(data.ListId, userId.Value);
+		if (userList == null)
+			return NotFound();
+
+		for (int i = 0; i < data.SortedIds.Count; i++)
+		{
+			var item = await _context.ListItems.FindAsync(data.SortedIds[i]);
+			if (item != null)
+			{
+				item.SortOrder = data.SortedIds.Count - i;
+			}
+		}
+
+		await _context.SaveChangesAsync();
+		return new JsonResult(new { success = true });
+	}
 
 }
